6d2a90bf0f60faaef55838ea025b8a9c1a9192d7
Lalit Sharma <lalit255290@gmail.com>
2019-03-18 21:37:18 -0400

    Add project files.

---
+++ Assignment2Group3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Assignment2Group3.MvcApplication" Language="C#" %>
---
+++ Assignment2Group3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assignment2Group3.Startup))]
+namespace Assignment2Group3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
